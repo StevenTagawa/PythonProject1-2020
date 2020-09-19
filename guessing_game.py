1302574275811efc4c1d17fc1078d3cb83cd3977
StevenTagawa <42248805+StevenTagawa@users.noreply.github.com>
2020-09-19 11:42:14 -0700

    Added features

--- guessing_game.py
+++ guessing_game.py
@@ -5,46 +5,39 @@ Project 1 - Number Guessing Game
 By Steven Tagawa
 September 2020
 """
+import os
 import random
 
-RANGE = 10
+RANGE = [10, 50, 100, 1000]
+MODE_NAMES = ["Easy", "Medium", "Hard", "INSANE"]
 
 
-def start_game():
-    answer = random.randint(1, RANGE)
+def start_game(mode):
+    answer = random.randint(1, RANGE[mode])
     guesses = 0
+    os.system("cls" if "name" == "nt" else "clear")
     print("Ready to play?\n")
-    if high_score:
+    if high_scores[mode]:
         print(
-            f"Your current high score is {high_score} {plural(high_score)}!\n")
+            f"Your current high score in {MODE_NAMES[mode].upper()} MODE is "
+            f"{high_scores[mode]} {plural(high_scores[mode])}!\n")
     else:
-        print("You haven't set a high score yet.\n")
+        print(
+            f"You haven't set a high score in {MODE_NAMES[mode].upper()} MODE "
+            f"yet.\n")
     while True:
-        guess = input(f"Guess a number between 1 and {RANGE}:  ")
+        guess = get_guess(guesses, answer, RANGE[mode])
         if not guess:
-            print(f"You didn't enter anything!  Try again.\n")
-            continue
-        try:
-            guess = int(guess)
-        except ValueError:
-            print(f"Oops... {guess} doesn't look like a number!  Try again.\n")
-            continue
-        # end try
-        if guess < 1 or guess > RANGE:
-            print(
-                f"Uh oh... {guess} isn't between 1 and {RANGE}.  Try again.\n")
-            continue
-        # end if
-        guesses += 1
-        if guess == answer:
-            print("Woo hoo!  You got it right!")
-            return guesses
-        elif guess < answer:
-            print("Nope!  Go higher!")
+            return None
         else:
-            print("Nope!  Go lower!")
-        # end if
-    # end while
+            guesses += 1
+            if guess == answer:
+                print("Woo hoo!  You got it right!")
+                return guesses
+            elif guess < answer:
+                print("Nope!  Go higher!")
+            else:
+                print("Nope!  Go lower!")
 
 
 def welcome():
@@ -54,57 +47,135 @@ def welcome():
     print("\nby Steven Tagawa")
     print("=" * 40)
     return
-# end def
 
 
 def goodbye():
-    print("\n", "=" * 40)
+    print("\n" + "=" * 40)
     print("Thanks for playing!  See you again soon!")
-    print("=" * 40, "\n")
+    print("=" * 40 + "\n")
     return
-# end def
 
 
-def plural(score):
-    if score > 1:
-        return "guesses"
-    else:
+def plural(number):
+    if number == 1:
         return "guess"
-    # end if
-# end def
+    else:
+        return "guesses"
 
 
-def check_score(score, high_score):
-    if high_score:
-        if score == high_score:
+def check_score(score, high_scores, mode):
+    if not score:
+        return high_scores
+    if high_scores[mode]:
+        if score == high_scores[mode]:
             print(
-                f"\nYou tied your best score of {high_score} "
-                f"{plural(high_score)}!")
-        elif score < high_score:
-            print("\nYou just beat your high score!")
-            print(f"Your new high score is {score} {plural(score)}!")
-            high_score = score
+                f"\nYou tied your best score in {MODE_NAMES[mode].upper()} "
+                f"MODE of {high_scores[mode]} {plural(high_scores[mode])}!")
+        elif score < high_scores[mode]:
+            print(
+                f"\nYou just beat your high score in "
+                f"{MODE_NAMES[mode].upper()} MODE!")
+            print(
+                f"Your new high score in {MODE_NAMES[mode].upper()} MODE is "
+                f"{score} {plural(score)}!")
+            high_scores[mode] = score
         else:
             print(f"\nYour score was {score} {plural(score)}.")
-        # end if
     else:
         print(f"\nYour score was {score} {plural(score)}.")
-        high_score = score
-    # end if
-    return high_score
-# end def
+        high_scores[mode] = score
+    return high_scores
 
 
-high_score = None
+def select_mode():
+    print("\n" + "=" * 40)
+    print("Select Difficulty Level\n")
+    for n, mode_name in enumerate(MODE_NAMES):
+        option = str(n+1)+". " + mode_name + f"(1-{RANGE[n]})"
+        print(
+            option + (" " * (22 - len(option))) + "[High Score: " +
+            str(high_scores[n]) + "]")
+    print("=" * 40 + "\n")
+    while True:
+        mode_choice = input("What's your choice? [1-4]  ")
+        if not check_empty(mode_choice):
+            continue
+        mode_choice = check_num(mode_choice)
+        if not mode_choice:
+            continue
+        if not check_range(mode_choice, 1, 4):
+            continue
+        return mode_choice - 1
+
+
+def check_num(string):
+    try:
+        number = int(string)
+    except ValueError:
+        print(f"Oops... {string} doesn't look like a number!  Try again.\n")
+        return None
+    return number
+
+
+def check_range(number, minimum, maximum):
+    if number < minimum or number > maximum:
+        print(
+            f"Uh oh... {number} isn't between {minimum} and {maximum}.  "
+            f"Try again.\n")
+        return False
+    else:
+        return True
+
+
+def check_empty(string):
+    if not string:
+        print(f"You didn't enter anything!  Try again.\n")
+        return False
+    else:
+        return True
+
+
+def check_quit(guess, guesses, answer):
+    if guess.lower() != "q":
+        return False
+    else:
+        print(f"The number was {answer}.")
+        print(f"You gave up after {guesses} {plural(guesses)}.")
+        return True
+
+
+def get_guess(guesses, answer, maximum):
+    while True:
+        print(f"Guess a number between 1 and {maximum}")
+        guess = input(f"or enter 'Q' to give up:  ")
+        if not check_empty(guess):
+            continue
+        if check_quit(guess, guesses, answer):
+            return None
+        guess = check_num(guess)
+        if not guess:
+            continue
+        if not check_range(guess, 1, maximum):
+            continue
+        return guess
+
 
 # EXECUTION STARTS HERE
+high_scores = [None, None, None, None]
+mode = None
+
 welcome()
 while True:
-    score = start_game()
-    high_score = check_score(score, high_score)
-    if input("\nDo you want to play again? [Y/N]  ").lower() != "y":
+    if not mode:
+        mode = select_mode()
+    score = start_game(mode)
+    high_scores = check_score(score, high_scores, mode)
+    if input("\nDo you want to play again? [Y/N]  ").lower() == "y":
+        if input(
+            "\nDo you want to change the difficulty level? [Y/N]  "
+        ).lower() == "y":
+            mode = None
+    else:
         break
-    # end if
-# end while
 goodbye()
 # EXECUTION ENDS HERE
